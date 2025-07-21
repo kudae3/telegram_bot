4bb09fa7bfb718f4227675b8200fd76b55859cc4
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-21 17:19:59 +0630

    feat: add quote fetching commands and enhance user interaction with inline keyboard

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Telegraf } from 'telegraf';
+import { Markup, Telegraf } from 'telegraf';
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { message } from 'telegraf/filters'
+import axios from 'axios';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ dotenv.config();
 const BOT_TOKEN = process.env.BOT_TOKEN as string;
 const WEBHOOK_URL = process.env.WEBHOOK_URL as string;
 const PORT = parseInt(process.env.PORT || '3000', 10);
+const ZENQUOTE_API = process.env.ZENQUOTE_API as string;
+const TODAY_ZENQUOTE_API = process.env.TODAY_ZENQUOTE_API as string;
 
 if (!BOT_TOKEN || !WEBHOOK_URL) {
   throw new Error('BOT_TOKEN and WEBHOOK_URL must be defined in .env');
@@ -19,14 +22,103 @@ const app = express();
 const bot = new Telegraf(BOT_TOKEN);
 
 // Bot commands
-bot.start((ctx) => {
+bot.start(async(ctx) => {
   const user = ctx.from?.username || 'Unknown User';
-  ctx.reply(`👋 Hello ${user}, Welcome to Our Bot!`);
+  await ctx.reply(`👋 Hello ${user}, Welcome to the Moti Hub Bot!`);
+  await ctx.reply('Please choose an option: ', 
+    Markup.inlineKeyboard([
+      Markup.button.callback('random Quote', 'random_quote'),
+      Markup.button.callback('Today Quote', 'today_quote'),
+      Markup.button.callback('Help', 'help')
+    ])
+  )
 });
 
 bot.help((ctx) => {
-  ctx.reply('Send me a message and I will echo it.');
+  ctx.reply('Send /start to receive a greeting');
+  ctx.reply('Send /keyboard to receive a message with a keyboard');
+  ctx.reply('Send /quit to stop the bot');
 });
+
+bot.command('/quote', async (ctx) => {
+  try {
+    const response = await axios.get(ZENQUOTE_API);
+    console.log('Quote fetched:', response.data);
+    
+    const quote = response.data[0].q;
+    const author = response.data[0].a;
+    ctx.reply(`💬 "${quote}" - ${author}`);
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+    ctx.reply('Sorry, I could not fetch a quote at the moment.');
+  }
+});
+
+bot.command('/main', (ctx) => {
+  ctx.reply(
+    'Please choose an option:',
+    Markup.keyboard([
+      ['Random Quote', 'Today Quote'],
+    ]).resize().oneTime()
+  );
+});
+
+bot.action('random_quote', async (ctx) => {
+  try {
+    const response = await axios.get(ZENQUOTE_API);
+    console.log('Random Quote fetched:', response.data);
+    const quote = response.data[0].q;
+    const author = response.data[0].a;
+    ctx.reply(`💬 "${quote}" - ${author}`);
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+    ctx.reply('Sorry, I could not fetch a quote at the moment.');
+  }
+})
+
+bot.action('today_quote', async (ctx) => {
+  try {
+    const response = await axios.get(TODAY_ZENQUOTE_API);
+    console.log('Toady Quote fetched:', response.data);
+    const quote = response.data[0].q;
+    const author = response.data[0].a;
+    ctx.reply(`💬 "${quote}" - ${author}`);
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+    ctx.reply('Sorry, I could not fetch a quote at the moment.');
+  }
+})
+
+bot.action('quit', (ctx) => {
+  ctx.reply('👋 Goodbye! If you need assistance, just type /start');
+})
+
+bot.hears('Random Quote', async (ctx) => {
+  try {
+    const response = await axios.get(ZENQUOTE_API);
+    console.log('Quote fetched:', response.data);
+    const quote = response.data[0].q;
+    const author = response.data[0].a;
+    ctx.reply(`💬 "${quote}" - ${author}`);
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+    ctx.reply('Sorry, I could not fetch a quote at the moment.');
+  }
+});
+
+bot.hears('Today Quote', async (ctx) => {
+  try {
+    const response = await axios.get(TODAY_ZENQUOTE_API);
+    console.log('Quote fetched:', response.data);
+    const quote = response.data[0].q;
+    const author = response.data[0].a;
+    ctx.reply(`💬 "${quote}" - ${author}`);
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+    ctx.reply('Sorry, I could not fetch a quote at the moment.');
+  }
+});
+
 
 bot.on(message('text'), (ctx) => {
   const user = ctx.from?.username || 'Unknown User';
