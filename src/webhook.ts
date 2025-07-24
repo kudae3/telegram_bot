63fdcf0cbd42b9184cd01b2f6f3507cb430db97f
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-24 18:35:23 +0630

    fix: remove unnecessary slash in webhook URL

--- src/webhook.ts
+++ src/webhook.ts
@@ -2,9 +2,9 @@ import { Telegraf } from "telegraf";
 import { env } from "./config/env";
 
 export const setupWebhook = (bot: Telegraf) => {
-    bot.telegram.setWebhook(`${env.WEBHOOK_URL}/telegram`)
+    bot.telegram.setWebhook(`${env.WEBHOOK_URL}telegram`)
     .then(async() => {
-        console.log(`Webhook set to ${env.WEBHOOK_URL}/telegram`)
+        console.log(`Webhook set to ${env.WEBHOOK_URL}telegram`)
         const info = await bot.telegram.getWebhookInfo()
         console.log('Current Webhook Info:', info);
     })
